e1ac77725f7c9a26edf9abdb887e89e8364c5a9a
Heinz-Th-Ch <69845390+Heinz-Th-Ch@users.noreply.github.com>
2020-10-19 09:25:35 +0200

    file migration completed

--- src/main/java/applications/CgaFileMigrationApplication.java
+++ src/main/java/applications/CgaFileMigrationApplication.java
@@ -1,12 +1,14 @@
 package applications;
 
 import dataStructures.CommonValues;
+import dataStructures.DataListContainerImpl;
 import enumerations.FileMigrationFunction;
 import enumerations.WorkingLevel;
 import utilities.ApplicationLoggerUtil;
 import utilities.PropertiesUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -17,7 +19,10 @@ public class CgaFileMigrationApplication {
     private static final int NUMBER_OF_ARGUMENTS = 2;
     private static final String PROPERTY_FILE_NAME = "CgaFileMigrationApplication.properties";
     private static final String RESOURCES = "resources";
+
     private static final Properties properties = new Properties();
+    private static final DataListContainerImpl dataListContainer = new DataListContainerImpl();
+
     private static WorkingLevel workingLevel;
     private static String property_file_path_and_name;
 
@@ -56,11 +61,50 @@ public class CgaFileMigrationApplication {
 
     private static void createDirectories() throws IOException {
         if (new File(properties.getProperty(CommonValues.PROPERTY_LOG_FILE_PATH)).mkdirs()) {
-            logger.info("directory path {} created", properties.getProperty(CommonValues.PROPERTY_LOG_FILE_PATH));
+            logger.info("log directory path {} created",
+                    properties.getProperty(CommonValues.PROPERTY_LOG_FILE_PATH));
+        }
+        if (new File(properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH)).mkdirs()) {
+            logger.info("data directory path {} created",
+                    properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH));
         }
     }
 
-    private static void createFiles() {
+    private static void createFiles() throws IOException {
+        if (!new File(properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH)).exists()) {
+            logger.error("data directory path {} don't exist. File creation is not possible",
+                    properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH));
+            return;
+        }
+
+        String actualDataPath = properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH);
+
+        File ballCharacteristicsFile = new File(actualDataPath +
+                properties.getProperty(CommonValues.PROPERTY_DATA_BALL_CHARACTERISTICS_FILE_NAME));
+        if (!ballCharacteristicsFile
+                .exists()) {
+            if (dataListContainer.storeBallCharacteristics(new FileOutputStream(ballCharacteristicsFile))) {
+                logger.info("ball characteristics file {} created", ballCharacteristicsFile);
+            }
+        }
+
+        File crazyGolfSiteCharacteristicsFile = new File(actualDataPath +
+                properties.getProperty(CommonValues.PROPERTY_DATA_CRAZY_GOLF_SITE_CHARACTERISTICS_FILE_NAME));
+        if (!crazyGolfSiteCharacteristicsFile
+                .exists()) {
+            if (dataListContainer.storeCrazyGolfSiteCharacteristics(new FileOutputStream(crazyGolfSiteCharacteristicsFile))) {
+                logger.info("crazy golf site characteristics file {} created", crazyGolfSiteCharacteristicsFile);
+            }
+        }
+
+        File suitCaseCharacteristicsFile = new File(actualDataPath +
+                properties.getProperty(CommonValues.PROPERTY_DATA_SUITCASE_CHARACTERISTICS_FILE_NAME));
+        if (!suitCaseCharacteristicsFile
+                .exists()) {
+            if (dataListContainer.storeSuitCaseCharacteristics(new FileOutputStream(suitCaseCharacteristicsFile))) {
+                logger.info("suitcase characteristics file {} created", suitCaseCharacteristicsFile);
+            }
+        }
 
     }
 
@@ -71,6 +115,9 @@ public class CgaFileMigrationApplication {
     private static void adjustProperties() {
         properties.setProperty(CommonValues.PROPERTY_LOG_FILE_PATH,
                 properties.getProperty(CommonValues.PROPERTY_LOG_FILE_PATH).replace(CommonValues.DIRECTORY_PLACE_HOLDER,
+                        workingLevel.getDirectoryName()));
+        properties.setProperty(CommonValues.PROPERTY_DATA_FILE_PATH,
+                properties.getProperty(CommonValues.PROPERTY_DATA_FILE_PATH).replace(CommonValues.DIRECTORY_PLACE_HOLDER,
                         workingLevel.getDirectoryName()));
     }
 
